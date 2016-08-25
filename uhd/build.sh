805f956a67c9aae4df33fe5bcc0a53a1aa57a486
Ryan Volz <rvolz@mit.edu>
2016-08-25 21:27:16 +0000

    Add "about" section to uhd recipe and run tests.

--- uhd/build.sh
+++ uhd/build.sh
@@ -7,8 +7,8 @@ cd build
 cmake ../ \
     -DBOOST_INCLUDEDIR=$PREFIX/include \
     -DBOOST_LIBRARYDIR=$PREFIX/lib \
-    -DBoost_NO_SYSTEM_PATHS=ON \
-    -DCMAKE_FIND_ROOT_PATH=$PREFIX \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX
 make
+make test
 make install
