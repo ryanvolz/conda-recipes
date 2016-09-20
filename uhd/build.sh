ccb30e2728b54da410bb99a020a7b1e3af92aafc
Ryan Volz <rvolz@mit.edu>
2016-09-20 13:22:17 -0400

    Make uhd compile with gcc>=5 with conda-forge using gcc<5.

--- uhd/build.sh
+++ uhd/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 source activate "${CONDA_DEFAULT_ENV}"
 
+# make builds with gcc>=5 compatible with conda-forge, currently using gcc<5
+CXXFLAGS="${CXXFLAGS} -D_GLIBCXX_USE_CXX11_ABI=0"
+
 cd host
 mkdir build
 cd build
 cmake ../ \
-    -DBOOST_INCLUDEDIR=$PREFIX/include \
-    -DBOOST_LIBRARYDIR=$PREFIX/lib \
+    -DBOOST_ROOT=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX
 make
