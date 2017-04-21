468fc47a52cb489279c7589038dddf9cf699b05c
Ryan Volz <rvolz@mit.edu>
2017-04-21 14:38:25 -0400

    digital_rf 2.4.0, including gr_drf. Delete old gr_drf recipe.

--- digital_rf/build.sh
+++ digital_rf/build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 source activate "${CONDA_DEFAULT_ENV}"
 
-sh autogen.sh
+# make builds with gcc>=5 compatible with conda-forge, currently using gcc<5
+CXXFLAGS="${CXXFLAGS} -D_GLIBCXX_USE_CXX11_ABI=0"
+
 mkdir build
 cd build
-../configure --prefix=${PREFIX}
+cmake ../ \
+    -DSWIG_EXECUTABLE=$PREFIX/bin/swig \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX
 make
-make check
-source/test_rf_write_hdf5
+make test
 rm -r /tmp/hdf5
 make install
-
-cd ..
-$PYTHON setup.py install
